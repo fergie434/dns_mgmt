0900d39a3bedc923bf54398698dbecb35dc198b1
fergie434 <fergie434@gmail.com>
2025-12-27 18:19:46 +0800

    Add tdns helper

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import os
-import tdnss
+import requests
 from dotenv import load_dotenv
-
+from tdns_helper import tdns_api
 load_dotenv()
 
-tdns_api_key=os.getenv('tdns_api_key')
-fortigate_api_key=os.getenv('fortigate_api_key')
+TDNS_API_KEY=os.getenv('tdns_api_key')
+TDNS_URL=os.getenv('tdns_url')
+FORTIGATE_API_KEY=os.getenv('fortigate_api_key')
+FORTIGATE_URL=os.getenv('fortigate_url')
 
 def main():
+    tdns = tdns_api(api_key=TDNS_API_KEY, api_url=TDNS_URL)
+    scopes = tdns.get_dhcp_scopes()
+
+    main_scope = None
+    for scope in scopes:
+        if scope.get('networkAddress') == '10.1.2.0':
+            main_scope = scope
+
+    reserved_leases = tdns.get_dhcp_scope(main_scope['name'])['reservedLeases']
     ...
 
 if __name__ == '__main__':
---
+++ tdns_helper.py
@@ -0,0 +1,37 @@
+import requests
+import os
+from urllib.parse import urljoin, parse_qs
+
+class tdns_api():
+    api_key=None
+    api_url=None
+    
+    def __init__(self, api_key, api_url):
+        if not self.api_key:
+            self.api_key = os.getenv('tdns_api_key')
+        if not self.api_url:
+            self.api_url = os.getenv('tdns_url')
+
+    def get_dhcp_scopes(self):
+        return self._get('/api/dhcp/scopes/list')['scopes']
+    
+    def get_dhcp_scope(self, scope_name):
+        params = {'name': scope_name}
+        return self._get('/api/dhcp/scopes/get', params)
+
+    def _build_request_url(self, path, extra_params=None) -> requests.models.PreparedRequest:
+        req = requests.models.PreparedRequest()
+        params = {'token': self.api_key}
+        if extra_params:
+            params = params | extra_params
+
+        req.prepare_url(urljoin(self.api_url, path), params)
+
+        return req.url
+
+    def _get(self, api_path, extra_params=None):
+        request_url = self._build_request_url(api_path, extra_params)
+        response = requests.get(request_url)
+
+        response.raise_for_status()
+        return response.json().get("response", [])
